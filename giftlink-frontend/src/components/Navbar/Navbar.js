00dc1d41ff4fe54a8f8cf768933f2a730c1d94eb
Nikita Gamit <nikitagamit30@gmail.com>
2026-05-07 21:57:28 +0530

    Update Navbar.js

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,18 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">GiftLink</a>
+            
+            {/* Brand */}
+            <Link className="navbar-brand" to="/app">
+                GiftLink
+            </Link>
+
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
 
+                    {/* Home / Main */}
                     <li className="nav-item">
-                    <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
+                        <Link className="nav-link" to="/app">
+                            Home
+                        </Link>
                     </li>
+
+                    {/* Gifts */}
                     <li className="nav-item">
-                        <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
+                        <Link className="nav-link" to="/app">
+                            Gifts
+                        </Link>
                     </li>
+
+                    {/* Login (optional but useful) */}
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/app/login">
+                            Login
+                        </Link>
+                    </li>
+
+                    {/* Register (optional) */}
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/app/register">
+                            Register
+                        </Link>
+                    </li>
+
                 </ul>
             </div>
         </nav>
