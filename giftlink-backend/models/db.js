4ea9dd8fce355daf575183084043d3410f31e161
Nikita Gamit <nikitagamit30@gmail.com>
2026-05-07 21:08:05 +0530

    Update db.js

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,28 +1,23 @@
+// db.js
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 
-const url = process.env.MONGO_URL;
-const dbName = 'giftDB';
+// MongoDB connection URL with authentication options
+let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
+const dbName = "giftdb";
 
 async function connectToDatabase() {
-    try {
-        const client = new MongoClient(url);
+	if (dbInstance){
+		return dbInstance
+	};
 
-        // Task 1: Connect to MongoDB
-        await client.connect();
+	const client = new MongoClient(url);
 
-        // Task 2: Connect to database giftDB
-        dbInstance = client.db(dbName);
-
-        // Task 3: Return database instance
-        return dbInstance;
-
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        throw error;
-    }
+	await client.connect();
+	dbInstance = client.db(dbName);
+	return dbInstance;
 }
 
 module.exports = connectToDatabase;
